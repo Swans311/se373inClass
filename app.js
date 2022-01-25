855c856d664abf9aede0072110a1bb1c3e7c6ed4
Swans311 <59575829+Swans311@users.noreply.github.com>
2022-01-24 22:16:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,58 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
-const port = process.env.port || 3000
+const mongoose = require('mongoose')
 const app = express()
+const parser = require('body-parser')
+const path = require('path')
 
-//set the view engine
-app.engine('hbs', exphbs.engine({
-    defaultLayout:'main',
-    extname:'.hbs',
-    helpers:{
-        getShortComment(comment){
-            if(comment.length < 60){
-                return comment
-            }
-            return comment.substring(0,60)+'...'
-        }
-    }
-}))
-app.set('view engine', 'hbs')
-//route to render the page
-app.get('/', (request, response)=>{
-    response.render('home', {
-        post:[{
-            author:"Jordan D",
-            image:"https://picsome.photos/500/500",
-            comments:["Comment 1", "comment 2", "Magna enim dolore veniam eiusmod do nulla id nulla ad id reprehenderit aliqua. Culpa consectetur exercitation quis cupidatat esse nostrud labore. Velit anim minim tempor aliqua aliqua proident voluptate laboris. Sint sunt occaecat magna pariatur laboris irure ex ut dolore cillum eiusmod qui cupidatat. Sit aliqua consequat sit nulla ipsum ut consequat tempor non incididunt do.", "Comment 100938478248932"]
+app.use(parser.json())
+app.use(parser.urlencoded({extended:true}))
+app.use(express.json())
 
-        },
-        {
-            author:"Shrek",
-            image:"https://picsome.photos/500/500?2",
-            comments:["Comment 1", "comment 4", "Pariatur mollit officia aliquip cillum mollit do cupidatat Lorem aliqua. Officia esse quis eu minim tempor dolore et quis sit enim nisi et laboris dolore. Pariatur velit id eiusmod aute excepteur officia pariatur ipsum laboris. Sint eu nulla ex minim. Pariatur exercitation proident qui excepteur pariatur qui est anim ut fugiat proident incididunt aliquip. Amet amet nisi ex ea dolor tempor. Ut velit enim exercitation et minim est proident ad incididunt magna quis."]
 
-        },{
-            author:"Godzilla",
-            image:"https://picsome.photos/500/500?30",
-            comments:["Good for an overgrown lizard, trash for anything else."]
 
-        }]
-    })
-
+mongoose.connect("mongodb://localhost:27017/foodEntries", {
+    useNewURLParser:true
+}).then(()=>{
+    console.log("Connected to mungo's db")
+}).catch((err)=>{
+    console.log(err)
 })
-//set up port for connection
-app.listen(port, ()=>{
-    console.log("Connected on port 3000")
+
+/*var Food = mongoose.model('Food', {typeOfFood:String})
+
+var food = new Food({typeOfFood:"Blue Milk from Star Wars"})
+
+food.save().then(()=>{
+    console.log("Food Saved")
+})*/
+
+require('./models/Food')
+var Food = mongoose.model('food')
+
+app.post('/saveFoodEntry',(request, response)=>{
+    console.log(request.body)
+    //create new entry for food
+    new Food(request.body).save().then(()=>{
+        console.log("Data saved")
+        response.redirect("foodList.html")
+    })
+})
+
+//read the data
+app.get('/getData', (req, res)=>{
+    Food.find().then((food)=>{
+        res.json({food})
+    })
+})
+
+//delete the data
+app.post('/deleteFood',(req, res)=>{
+    console.log("Food Item Deleted" + req.body._id)
+    Food.findByIdAndDelete(req.body._id).exec()
+    res.redirect('foodList.html')
+})
+
+app.use(express.static(__dirname+"/views"))
+app.listen(3000,()=>{
+    console.log("Listening mungo 3000")
 })
